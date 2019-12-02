f75e92a0c6d5243b0e1b372aae48881830e36949
telliottosceola <travis@controlanything.com>
2019-12-02 16:41:34 -0600

    Update mcp23008-i2cdio8.ino

--- mcp23008-i2cdio8.ino
+++ mcp23008-i2cdio8.ino
@@ -3,6 +3,8 @@
 
 MCP23008 inputBoard;
 
+int previousInputState;
+
 void setup() {
     inputBoard.setAddress(1,0,0);
     if(!inputBoard.initialized){
@@ -13,5 +15,13 @@ void setup() {
 }
 
 void loop() {
-    Serial.printf("Input Status %i \n", inputBoard.readAllInputs());
+    //And with 1 to only monitor input 1 state, disreguard all other inputs.
+    int status = inputBoard.readAllInputs()&1;
+    if(status != previousInputState){
+        if(status > previousInputState){
+            //input is high
+        }else{
+            //input is low
+        }
+    }
 }
